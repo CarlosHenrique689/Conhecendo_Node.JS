c2a05b8bf50a9154bc0c83b34c8c042c8b07186b
Carlos Henrique Alves dos Santos <chads0rei10@gmail.com>
2024-12-05 18:33:50 -0300

    Melhorando os Exercicios de Testes!

--- Exercicios de Testes/CalcularLitros.js
+++ Exercicios de Testes/CalcularLitros.js
@@ -1,13 +1,15 @@
 function LitrosNecessários(distânciaPercurso, tipoCombustível){
-    if(tipoCombustível === "gasolina"){
-        resultado = distânciaPercurso / 16
-        return resultado
-    }
-    else if(tipoCombustível === "etanol"){
-        resultado = distânciaPercurso / 11
-        return resultado
-
-    }else{return "Tipo de combustível inválido"}
+    if(Number.isInteger(distânciaPercurso)){
+        if(distânciaPercurso > 0){
+            if(tipoCombustível === "gasolina"){
+                resultado = distânciaPercurso / 16
+                return resultado}
+            else if(tipoCombustível === "etanol"){
+                resultado = distânciaPercurso / 11
+                return resultado
+            }else{return "Tipo de combustível inválido"}
+        }else{return "Distância do Percurso inválida"}
+    }else{return "Distância do Percurso inválida"}
 };
 
 module.exports = {LitrosNecessários};
--- Exercicios de Testes/CalcularLitros.test.js
+++ Exercicios de Testes/CalcularLitros.test.js
@@ -5,9 +5,21 @@ test('Tipo de combustível escolhido: gasolina', () => {
 });
 
 test('Tipo de combustível escolhido: etanol', () => {
-    expect(calcularLitros.LitrosNecessários(22000, "etanol")).toBe(2000);
+  expect(calcularLitros.LitrosNecessários(22000, "etanol")).toBe(2000);
 });
 
 test('Tipo de combustível escolhido: diessel', () => {
-    expect(calcularLitros.LitrosNecessários(20000, "Diessel")).toBe("Tipo de combustível inválido");
+  expect(calcularLitros.LitrosNecessários(20000, "Diessel")).toBe("Tipo de combustível inválido");
+});
+
+test('Distância do percurso não inteira ', () => {
+  expect(calcularLitros.LitrosNecessários(10000.8, "gasolina")).toBe("Distância do Percurso inválida");
+});
+
+test('Distância do percurso negativa', () => {
+  expect(calcularLitros.LitrosNecessários(-10000, "gasolina")).toBe("Distância do Percurso inválida");
+});
+
+test('Distância do percurso não tipo Number', () => {
+  expect(calcularLitros.LitrosNecessários("10000", "gasolina")).toBe("Distância do Percurso inválida");
 });
